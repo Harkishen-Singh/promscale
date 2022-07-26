15df0e0227c3f24b2fd01d6394ce1ad1659521a1
Harkishen-Singh <harkishensingh@hotmail.com>
2022-07-26 14:35:51 +0530

    Benchmark trace querying with tags applied with and without double quotes.
    Signed-off-by: Harkishen-Singh <harkishensingh@hotmail.com>

---
+++ pkg/tests/end_to_end_tests/trace_query_benchmark_test.go
@@ -0,0 +1,56 @@
+package end_to_end_tests
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"net/http"
+	"testing"
+
+	"github.com/jackc/pgx/v4/pgxpool"
+	"github.com/stretchr/testify/require"
+
+	promscaleJaeger "github.com/timescale/promscale/pkg/jaeger"
+	jaegerquery "github.com/timescale/promscale/pkg/jaeger/query"
+	ingstr "github.com/timescale/promscale/pkg/pgmodel/ingestor"
+	"github.com/timescale/promscale/pkg/pgxconn"
+)
+
+func BenchmarkTraceQueryWithTags(b *testing.B) {
+	sampleTraces := generateTestTrace()
+
+	withDB(b, "bench_e2e_trace_query_with_tags", func(db *pgxpool.Pool, t testing.TB) {
+		ingestor, err := ingstr.NewPgxIngestorForTests(pgxconn.NewPgxConn(db), nil)
+		require.NoError(t, err)
+		defer ingestor.Close()
+
+		// Ingest traces into Promscale.
+		err = ingestor.IngestTraces(context.Background(), copyTraces(sampleTraces))
+		require.NoError(t, err)
+
+		// Start Promscale's HTTP endpoint for Jaeger query.
+		router, _, err := buildRouter(db)
+		require.NoError(t, err)
+		jaegerQuery := jaegerquery.New(pgxconn.NewPgxConn(db), &jaegerquery.DefaultConfig)
+		promscaleJaeger.ExtendQueryAPIs(router, pgxconn.NewPgxConn(db), jaegerQuery)
+
+		// Bind to the server port. This must be outside of the goroutine below
+		// to prevent the server bind and client connect from racing.
+		listener, err := net.Listen("tcp", ":0")
+		require.NoError(t, err)
+
+		go func() {
+			server := http.Server{Handler: router}
+			require.NoError(t, server.Serve(listener))
+		}()
+
+		promscaleClient := httpClient{"http://" + listener.Addr().String()}
+
+		for _, tc := range traceQueryCases {
+			b.Run(fmt.Sprintf("case_%s", tc.name), func(b *testing.B) {
+				b.ReportAllocs()
+				getTraces(t, promscaleClient, tc.service, tc.start, tc.end, tc.tag)
+			})
+		}
+	})
+}
